d65a4236f83b99b1b744a2cd5cb94f5b0b9f0a42
Zsofia <you@example.com>
2023-03-07 22:50:36 +0100

    commit

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 
-const ProductCard = () => ({product, setCount, count}) {
+export function ProductCard({product, setCount, count}){
     
-  
 
     return(
       <div className="product-card">
